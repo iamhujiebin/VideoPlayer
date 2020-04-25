9265fe24d64a0b048058a8e6785577f76b09b398
jiebin.hu <jiebin.hjb@nonolive.com>
2020-04-25 14:47:42 +0800

    添加项目文件。

---
+++ VideoPlayer/VideoPlayer.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+
+#define SDL_MAIN_HANDLED
+extern "C"
+{
+#include "SDL.h"
+#include "libavcodec/avcodec.h"
+#include "libavformat/avformat.h"
+#include "libswscale/swscale.h"
+#include "libavutil/imgutils.h"
+};
+
+int main()
+{
+	av_register_all();
+	if (SDL_Init(SDL_INIT_VIDEO)) {
+		printf("Could not initialize SDL - %s\n", SDL_GetError());
+		return -1;
+	}
+    std::cout << "Hello World!\n";
+}
